33d0c2dea5292ee650b58b7bcf0059b5ccd7489a
Bidyut mazumder <46864109+bidyut102@users.noreply.github.com>
2022-01-17 23:41:23 +0600

    Initial commit

---
+++ lib/categoryitem.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  CategoryItem(this.title, this.color);
+
+  final String title;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
